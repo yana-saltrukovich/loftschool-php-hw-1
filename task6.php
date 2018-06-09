d96be2ea243a8ba6dca6f0f012a3c96c66f3a680
HannaMiru <37253045+yana-saltrukovich@users.noreply.github.com>
2018-06-09 12:13:32 +0500

    Update task6.php

--- task6.php
+++ task6.php
@@ -1,24 +1,32 @@
-<table>
-    <?php
-    for ($i = 1; $i < 10; $i++) {
-        for ($j = 1; $j < 10; $j++) {
-            $num = $i * $j . " ";
-            if (($i % 2) == 0) {
-                if (($j % 2) == 0) {
-                    echo "[" . $num . "]";
-                } else {
-                    echo $num;
-                }
-            } else {
-                if (($j % 2) == 0) {
-                    echo $num;
-                } else {
-                    echo "(" . $num . ")";
-                }
-            }
-        }
-        echo "<br>";
-        $j = 1;
-    }
-    ?>
+<table border="1">
+   <?php
+   for ($i = 1; $i < 10; $i++) {
+
+       echo "<tr>";
+       for ($j = 1; $j < 10; $j++) {
+
+           echo "<td>";
+           $num = $i * $j . " ";
+
+
+           if (($i % 2) == 0) {
+               if (($j % 2) == 0) {
+                   echo "(" . $num . ")";
+               } else {
+                   echo $num;
+               }
+           } else {
+               if (($j % 2) == 0) {
+                   echo $num;
+               } else {
+                   echo "[" . $num . "]";
+               }
+           }
+           echo "</td>";
+       }
+       echo "</tr>";
+       echo "<br>";
+       $j = 1;
+   }
+   ?>
 </table>
